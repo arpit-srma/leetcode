950d2d8930917bcfb0c0b719d0d5c99eff177b14
arpit-srma <146662251+arpit-srma@users.noreply.github.com>
2023-10-23 00:48:33 +0530

    Time: 15 ms (16.77%), Space: 19.5 MB (5.55%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+    set<int>s(nums.begin(),nums.end());
+      
+      int j=0;
+        for(auto i:s)
+        { 
+            nums[j++]=i;
+        }
+        return j;
+    }
+};
+  
